7088b13a4960d82675e6856c4b7b95a17ae70e28
Seb Jensen <36541481+sebjensen@users.noreply.github.com>
2020-02-11 16:45:19 +0000

    robot-config.cpp

---
+++ src/robot-config.cpp
@@ -0,0 +1,28 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+controller Controller1 = controller(primary);
+motor FrontLeft = motor(PORT12, ratio18_1, false);
+motor BackLeft = motor(PORT11, ratio18_1, false);
+motor FrontRight = motor(PORT2, ratio18_1, false);
+motor BackRight = motor(PORT1, ratio18_1, false);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
